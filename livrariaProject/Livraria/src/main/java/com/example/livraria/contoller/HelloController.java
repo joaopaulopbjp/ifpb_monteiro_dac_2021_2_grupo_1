4727c1fd5fc5c25f1701b7b2da0b5e4e38f015c2
JordielsonSilva <jordielson.emanuel@academico.ifpb.edu.br>
2021-11-20 20:30:04 -0300

    add staly index

--- livrariaProject/Livraria/src/main/java/com/example/livraria/contoller/HelloController.java
+++ livrariaProject/Livraria/src/main/java/com/example/livraria/contoller/HelloController.java
@@ -20,7 +20,7 @@ public class HelloController {
 	public String hello(Model model) {
 		List<Autor> listaAutores = autorService.getAll();
 		model.addAttribute("autores", listaAutores);
-		return "index";
+		return "fragments";
 	}
 
 	@GetMapping("/cadastrar-endereco")
